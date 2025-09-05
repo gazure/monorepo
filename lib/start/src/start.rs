0101302a5912a2e9777a3fb08c10b4c19b3a8842
Grant Azure <azure.grant@gmail.com>
2025-09-04 23:07:22 -0700

    [start] use tracingx

--- lib/start/src/start.rs
+++ lib/start/src/start.rs
@@ -9,9 +9,9 @@ pub fn start(app: fn() -> dioxus::prelude::Element) {
 
     tracingx::init_logging();
     let app_meta = AppMeta::from_env();
-    let root_span = tracing::info_span!("app", app = %app_meta.app, region = %app_meta.region, host = %app_meta.host);
+    let root_span = tracingx::info_span!("app", app = %app_meta.app, region = %app_meta.region, host = %app_meta.host);
     root_span.in_scope(|| {
-        tracing::info!(port = 8080, address = "0.0.0.0", "Server started successfully");
+        tracingx::info!(port = 8080, address = "0.0.0.0", "Server started successfully");
         dioxus::launch(app);
     });
 }
