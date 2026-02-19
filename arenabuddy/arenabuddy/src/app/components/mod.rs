3326d4ad14db5abd98e0ea50d22222fa0cfcfb19
Grant Azure <azure.grant@gmail.com>
2026-02-18 19:21:44 -0800

    [arenabuddy] add pagination to the matches/drafts page

--- arenabuddy/arenabuddy/src/app/components/mod.rs
+++ arenabuddy/arenabuddy/src/app/components/mod.rs
@@ -3,9 +3,11 @@ mod deck_list;
 mod event_log;
 mod match_info;
 mod mulligan_display;
+mod pagination;
 
 pub use cost::ManaCost;
 pub use deck_list::DeckList;
 pub use event_log::EventLogDisplay;
 pub use match_info::MatchInfo;
 pub use mulligan_display::MulliganDisplay;
+pub use pagination::Pagination;
