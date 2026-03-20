435ef3ae38f96607d47590ce5749cd8fd9eb2273
Grant Azure <azure.grant@gmail.com>
2026-03-19 21:13:01 -0700

    [arenbuddy] add classification to the client experience

--- arenabuddy/core/src/services/match_service.rs
+++ arenabuddy/core/src/services/match_service.rs
@@ -1,4 +1,5 @@
 pub use crate::proto::arenabuddy::api::v1::{
-    DeleteMatchRequest, DeleteMatchResponse, GetMatchDataRequest, GetMatchDataResponse, ListMatchesRequest,
-    ListMatchesResponse, UpsertMatchDataRequest, UpsertMatchDataResponse, match_service_client, match_service_server,
+    ArchetypeClassification, ClassifyMatchRequest, ClassifyMatchResponse, DeleteMatchRequest, DeleteMatchResponse,
+    GetMatchDataRequest, GetMatchDataResponse, ListMatchesRequest, ListMatchesResponse, UpsertMatchDataRequest,
+    UpsertMatchDataResponse, match_service_client, match_service_server,
 };
